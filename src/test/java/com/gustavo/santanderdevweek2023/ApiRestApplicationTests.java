0886c795b5dccbe7fa793708ce5278d57d763e13
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-03-12 16:06:59 -0300

    Initial commit

---
+++ src/test/java/com/gustavo/santanderdevweek2023/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.santanderdevweek2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
